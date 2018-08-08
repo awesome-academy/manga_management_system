ed8c72aaa1e3066f6d3a2244a5fda25b564daba2
NHDang95 <2d2l95@gmail.com>
2018-08-08 20:54:42 +0700

    user devise/authenticate social

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable, :timeoutable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :rates
   has_many :follows
   has_many :comments
   has_many :likes
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.skip_confirmation!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users, only[:index, :show]
+  devise_for :users, controllers: { sessions: "users/sessions",
+                                    registrations: "users/registrations",
+                                    omniauth_callbacks: "users/omniauth" }
+  devise_scope :user do
+    get "sign_up", to: "devise/registrations#new"
+    get "edit", to: "devise/registrations#edit"
+    get "sign_in", to: "devise/sessions#new"
+  end
 end
